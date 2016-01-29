6a4c57d99441c92cbd4c3d47b3c9e1c041a9a5a8
cadovnik <hotchenkov@list.ru>
2016-01-29 14:16:03 +0300

    matrix example from interview

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+
+void islandFind(int** M,int n,int m){
+	
+	struct point{
+		int i=0;
+		int j=0;
+		bool left=false, right=false,up=false, down=false;
+		void print(){
+			cout<<"i:"<<i<<" j:"<<j<<" "
+			<<endl
+			<<"up "<<up<<endl
+			<<"down "<<down<<endl
+			<<"left "<<left<<endl
+			<<"right "<<right<<endl;
+		}
+	};
+	vector<point> v_points;
+
+	for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				if((int)M[i*m+j]){
+					  //  cout<<"i:"<<i<<" j:"<<j<<" ";
+					    point one;
+					    one.i=i;
+					    one.j=j;
+					  if(i>0 && (int) M[(i-1)*m+j]){
+//						cout<<"up one ";					
+						one.up=true;
+					} if(i<(n-1) && (int)M[(i+1)*m+j]){
+//						cout<<"down one ";			
+						one.down=true;						
+					} if(j<(m-1) && (int)M[i*m+(j+1)]){
+//						cout<<"right one ";
+						one.right=true;
+					} if(j>0 && (int)M[i*m+(j-1)]){
+//						cout<<"left one ";
+						one.left=true;
+					}
+					v_points.push_back(one);
+				//	one.print();
+			}
+						
+			}
+				
+		
+			cout<<endl;
+		}
+			size_t v_size_p=v_points.size();
+
+			if(v_size_p){
+				for(int i=0;i<v_size_p;i++){
+				point local=v_points.at(i);
+				local.print();
+				cout<<endl;
+					
+				}
+				
+	}
+}
+
+
+int main(int argc, char** argv) {
+	
+	int M[5][5]={
+	{0,0,1,1,1	},
+	{0,0,0,1,1	},
+	{1,0,0,0,1	},
+	{1,0,0,0,0	},
+	{0,0,0,1,0	}
+	};
+	
+	
+	islandFind((int**)M,5,5);
+	
+	return 0;
+}
